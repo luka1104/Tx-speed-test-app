e903b9f577b8e65ab221aeac95c234a00586e9de
luka1104 <skyward.luke11@gmail.com>
2022-08-08 19:09:53 +0900

    add tx hash

--- tx-speed-test/pages/index.tsx
+++ tx-speed-test/pages/index.tsx
@@ -2,26 +2,31 @@ import type { NextPage } from 'next'
 import React from 'react'
 import { Center, Grid } from '@chakra-ui/react'
 import Card from '../src/components/card'
+import Navbar from '../src/components/navbar'
 
 const Home: NextPage = () => {  
   return (
     <>
+      <Navbar />
       <Center>
         <Grid gridTemplateColumns={{sm: 'repeat(1, 1fr)', lg: 'repeat(3, 1fr)'}} gap={6}>
           <Card
             name="ethereum"
             icon="https://s2.coinmarketcap.com/static/img/coins/200x200/1027.png"
             img="https://www.coindeskjapan.com/wp-content/uploads/2022/01/shutterstock_776426233-710x458.jpg"
+            scanURI="https://goerli.etherscan.io/tx/"
           ></Card>
           <Card
             name="polygon"
             icon="https://cdn.todayscrypto.news/wp-content/uploads/2022/03/1.polygon-matic-logo.png"
             img="https://bittimes.net/wp-content/uploads/2022/05/Polygon-MATIC-Logo-450.jpg"
+            scanURI="https://mumbai.polygonscan.com/tx/"
           ></Card>
           <Card
             name="ohaiyo"
             icon="https://cdn-images-1.medium.com/max/1200/1*aEUc1bvEC4etWj6JTg6VMg.png"
             img="https://imgs.coinpost-ext.com/uploads/2022/07/aptos-ftx.jpeg"
+            scanURI="https://mumbai.polygonscan.com/tx/"
           ></Card>
         </Grid>
       </Center>
--- tx-speed-test/src/components/card.jsx
+++ tx-speed-test/src/components/card.jsx
@@ -8,24 +8,36 @@ import {
     Text,
     Stack,
     Button,
+    Link,
     useColorModeValue,
   } from '@chakra-ui/react';
   import React, { useState } from 'react'
   import { useStopwatch } from "react-timer-hook";
   import { toast } from 'react-toastify'
+  import BeatLoader from "react-spinners/BeatLoader";
+  import { HiOutlineExternalLink } from 'react-icons/hi'
   
   export default function SocialProfileWithImage(props) {
-    // const [loading, setLoading] = useState(false);
-    // const [polygonTimer, setPolygonTimer] = useState(0);
+    const [loading, setLoading] = useState(false);
+    const [txHash, setTxHash] = useState('');
     const { seconds, minutes, hours, isRunning, start, pause, reset } = useStopwatch({ autoStart: false });
+    const shortenString = (str, n) => {
+      return (str.length > n) ? str.slice(0, n-1) + '...' : str;
+    }
     const handleTransfer = async () => {
+      const res = await fetchAPI();
+      console.log(res);
+      setTxHash(res.receipt.transactionHash);
+    }
+    const fetchAPI = async () => {
+      setLoading(true);
       start();
       toast('transaction sended!')
       const data = {
         'address': "0x50B80aa3877fC852f3194a0331177FDDcF0891bf",
         'chain': props.name
       }
-      fetch(`/api/transfer`, {
+      return fetch(`/api/transfer`, {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
@@ -34,9 +46,16 @@ import {
       })
       .then(resp => {
         if(resp.status === 200) {
+          setLoading(false);
           pause();
           toast('transaction confirmed!')
+          const data = resp.json()
+          console.log(data);
+          return data;
         }
+      }).catch(e => {
+        console.log(e);
+        return e;
       })
     }
     return (
@@ -80,26 +99,46 @@ import {
                 </Text>
               </Stack>
               <Stack spacing={0} align={'center'}>
-                <Text fontWeight={600}>s</Text>
+                {txHash ? (
+                  <Link href={`${props.scanURI}${txHash}`} fontWeight={600}>{shortenString(txHash, 6)}</Link>
+                ) : (
+                  <Text fontWeight={600}>-</Text>
+                )}
                 <Text fontSize={'sm'} color={'gray.500'}>
-                  Deploy speed
+                  Tx Hash
                 </Text>
               </Stack>
             </Stack>
-  
-            <Button
-              onClick={handleTransfer}
-              w={'full'}
-              mt={8}
-              bg={useColorModeValue('#151f21', 'gray.900')}
-              color={'white'}
-              rounded={'md'}
-              _hover={{
-                transform: 'translateY(-2px)',
-                boxShadow: 'lg',
-              }}>
-              Send Tx
-            </Button>
+            {loading ? (
+              <Button
+                disabled
+                w={'full'}
+                mt={8}
+                bg={useColorModeValue('#151f21', 'gray.900')}
+                color={'white'}
+                rounded={'md'}
+                _hover={{
+                  transform: 'translateY(-2px)',
+                  boxShadow: 'lg',
+                }}
+              >
+                <BeatLoader color={"#A9A9A9"} />
+              </Button>
+            ) : (
+              <Button
+                onClick={handleTransfer}
+                w={'full'}
+                mt={8}
+                bg={useColorModeValue('#151f21', 'gray.900')}
+                color={'white'}
+                rounded={'md'}
+                _hover={{
+                  transform: 'translateY(-2px)',
+                  boxShadow: 'lg',
+                }}>
+                Send Tx
+              </Button>
+            )}
           </Box>
         </Box>
       </Center>
